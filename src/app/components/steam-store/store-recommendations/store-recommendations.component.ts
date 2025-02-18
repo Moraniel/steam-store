2f9984f133fc0706b10dfac1cbf120d7df3aa3d1
Moraniel <moraniel@outlook.com>
2025-02-18 09:35:35 -0300

    Adicionando carrosel nos destaques

--- src/app/components/steam-store/store-recommendations/store-recommendations.component.ts
+++ src/app/components/steam-store/store-recommendations/store-recommendations.component.ts
@@ -1,15 +1,19 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+
+
 
 @Component({
   selector: 'app-store-recommendations',
-  imports: [NgFor],
+  imports: [NgFor,CarouselModule],
   templateUrl: './store-recommendations.component.html',
   styleUrl: './store-recommendations.component.css'
 })
 export class StoreRecommendationsComponent {
   
   game_data:any = {
+    "id":0,
     "name": "Baldur's Gate 3",
     "shopURL":"https://store.steampowered.com/app/1086940/Baldurs_Gate_3/",
     "images": {
@@ -26,16 +30,34 @@ export class StoreRecommendationsComponent {
     "tag": "Popular",
     "preco":"R$ 230,00"
 }
+games:any[]=[this.game_data];
 
-
-
-  frontBack(){
-    setTimeout(() => {
-      this.game_data.images.cover = this.game_data.images.front_img;
-    }, 280);
+  constructor(){
+    this.games.push({
+      "id":1,
+      "name": "Sea of Thieves",
+      "shopURL":"https://store.steampowered.com/app/1172620/Sea_of_Thieves_2024_Edition/",
+      "images": {
+        "front_img":"./../imgs/games/sot/sot-front.jpg",
+          "cover": "./../imgs/games/sot/sot-front.jpg",
+          "screenshots": [
+              "./../imgs/games/sot/sot-extra.jpg",
+              "./../imgs/games/sot/sot-extra-1.jpg",
+              "./../imgs/games/sot/sot-extra-2.jpg",
+              "./../imgs/games/sot/sot-extra-3.jpg"
+          ]
+      },
+      "status": "Já Disponível",
+      "tag": "Popular",
+      "preco":"R$ 230,00"
+  })
   }
-  changeFront(img:string){
-
-    this.game_data.images.cover = img
+  frontBack(id:number){
+    
+    this.games[id].images.cover = this.games[id].images.front_img;
+    
+  }
+  changeFront(img:string,id:number){
+    this.games[id].images.cover = img
   }
 }
